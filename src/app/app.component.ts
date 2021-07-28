9a0ce9f26c44842796bc5c01096335b3df912119
meghatechclub <megha.techclub@kluniversity.in>
2021-07-28 11:07:14 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ServiceRequest';
+  form = new FormGroup({
+    name: new FormControl('',[Validators.required, Validators.minLength(5)]),
+    body: new FormControl('',[Validators.required, Validators.minLength(50)])
+  })
+  get f(){
+    return this.form.controls;
+  }
+  submit()
+  {
+    console.log(this.form.value)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatToolbar, MatToolbarModule} from '@angular/material/toolbar'
+import {MatIconModule} from '@angular/material/icon'
+import {MatSliderModule} from '@angular/material/slider'
+import {MatButtonModule} from '@angular/material/button'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ResponsesComponent } from './responses/responses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResponsesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSliderModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
